5fb3cc67b26b8553ac33d3de21fb84efea01b7f8
Irina Bugrova <irina92-08@mail.ru>
2025-05-24 22:36:41 +0300

    eslint fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,2 @@
-import  '../css/style.scss';
-import  'bootstrap';
+import '../css/style.scss'
+import 'bootstrap'
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   root: resolve(__dirname, 'src'),
   server: {
     port: 8080,
-    hot: true
-  }
+    hot: true,
+  },
 })
