ddf221894b1b8d9262caa7ef6b8ff0457645a734
Irina Bugrova <irina92-08@mail.ru>
2025-06-10 00:32:02 +0300

    modified mvc and style html, added eslintignore

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
+  {
+    ignores: [
+      'node_modules/**',
+      '**/dist/**',
+
+    ],
+  },
   stylistic.configs.recommended,
   { files: ['/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
--- src/js/view.js
+++ src/js/view.js
@@ -1,15 +1,25 @@
+import onChange from 'on-change'
 
-const view = (validate, url, feeds,state) => {
-    validate(url, feeds)
-    .then((data) => {
-        console.log(data);
-        state.elementsForm.input.classList.remove('is-invalid')
-        state.feeds.push(url);
-        state.elementsForm.p.textContent = '';
+const view = (validate, url, feeds, state) => {
+  const watchedState = onChange(state, (path, value) => {
+    if (value) {
+      state.elementsForm.input.classList.remove('is-invalid')
+      state.elementsForm.p.textContent = ''
+    }
+    else {
+      state.elementsForm.input.classList.add('is-invalid')
+    }
+  })
+
+  validate(url, feeds)
+    .then(() => {
+      console.log('ok')
+      state.feeds.push(url)
+      watchedState.stateInvalid = true
     })
     .catch((err) => {
-        state.elementsForm.input.classList.add('is-invalid');
-        state.elementsForm.p.textContent = err.message;
+      watchedState.stateInvalid = false
+      state.elementsForm.p.textContent = err.message
     })
 }
-export default view;
+export default view
